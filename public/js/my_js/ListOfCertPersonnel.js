2571981e2b418428e608c7bdc69efa4cc37b3f88
Christian Pagtalunan <christianpagtalunan@gmail.com>
2026-08-07 16:18:13 +0800

    Changes:: Per position, series filter and export

--- public/js/my_js/ListOfCertPersonnel.js
+++ public/js/my_js/ListOfCertPersonnel.js
@@ -11,19 +11,24 @@ $(document).ready(function () {
                 $('#selCertPersonnelSection').html('');
                 $('#selCertPersonnelSeries').html('');
                 $('#selCertPersonnelProductLine').html('');
+                $('#selCertPersonnelPosition').html('');
             },
             success: function(response) {
                 let section = response.section;
                 let prodLine = response.product_line;
                 let series = response.series;
+                let position = response.position;
 
                 let sectionOption = "";
                 let prodLineOption = "";
                 let seriesOption = "";
+                let positionOption = "";
 
                 sectionOption += `<option value="" selected disabled>Select Section</option>`;
                 prodLineOption += `<option value="" selected disabled>Select Product Line</option>`;
                 seriesOption += `<option value="" selected disabled>Select Series</option>`;
+                positionOption += `<option value="" selected disabled>Select Position</option>`;
+
                 section.forEach(function(item) {
                     sectionOption += `<option value="${item}">${item}</option>`;
                 });
@@ -40,6 +45,12 @@ $(document).ready(function () {
                     seriesOption += `<option value="${item}">${item}</option>`;
                 })
                 $('#selCertPersonnelSeries').append(seriesOption);
+
+                position.forEach(function(item) {
+                    positionOption += `<option value="${item}">${item}</option>`;
+                });
+                $('#selCertPersonnelPosition').append(positionOption);
+                
             },
             error: function(xhr, status, error) {
                 toastr.error('Error fetching list of certified personnel. Please try again later.');
@@ -50,14 +61,15 @@ $(document).ready(function () {
 
     $("#btnExportListCertPersonnel").on('click', function() {
         let section = $('#selCertPersonnelSection').val();
-        // let series = $('#selCertPersonnelSeries').val();
         let productLine = $('#selCertPersonnelProductLine').val();
+        let series = $('#selCertPersonnelSeries').val();
+        let position = $('#selCertPersonnelPosition').val();
 
         if (!section || !productLine) {
             toastr.warning('Please select both Section and Product Line before exporting.');
             return;
         }
 
-        window.open(`export_list_cert_personnel?section=${section}&product_line=${productLine}`, '_blank');
+        window.open(`export_list_cert_personnel?section=${section}&product_line=${productLine}&series=${series}&position=${position}`, '_blank');
     });
 });
