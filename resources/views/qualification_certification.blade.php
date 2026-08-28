32df4f16d6b9f46a946f72c32d786b265c2f50d3
Miguel C. Legaspi <mclegaspi@pricon.ph>
2026-08-28 09:55:27 +0800

    QC Change Module Name

--- resources/views/qualification_certification.blade.php
+++ resources/views/qualification_certification.blade.php
@@ -41,7 +41,7 @@
                                     <div class="d-flex justify-content-between align-items-center flex-wrap">
                                         <div class="mb-2 mb-md-0">
                                             <p class="text-uppercase text-muted small mb-1">Certification workspace</p>
-                                            <h5 class="card-title mb-0 text-secondary">Qualification / Validation</h5>
+                                            <h5 class="card-title mb-0 text-secondary">Certification / Validation</h5>
                                         </div>
                                         <button type="button" id="btnCreateCQForm" class="btn btn-primary" data-toggle="modal" data-target="#modalCreateCQForm"><i class="fa fa-plus fa-md mr-2"></i>Certify Employee</button>
                                     </div>
@@ -133,7 +133,7 @@
             <div class="modal-dialog modal-dialog-scrollable modal-xl" style="width: 95% !important; min-width: 95% !important;">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title mb-0" id="createCQFormLabel">Qualification / Validation Form</h5>
+                        <h5 class="modal-title mb-0" id="createCQFormLabel">Certification / Validation Form</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
                             </button>
                     </div>
--- resources/views/shared/pages/super_user_nav.blade.php
+++ resources/views/shared/pages/super_user_nav.blade.php
@@ -167,7 +167,7 @@
                     <li class="nav-item has-treeview">
                         <a href="{{ route('qualification_certification') }}" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
-                            <p>Qualification / Validation</p>
+                            <p>Qualification / Certification</p>
                         </a>
                     </li>
                     {{-- @endif --}}
