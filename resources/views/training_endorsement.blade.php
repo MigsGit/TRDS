fe722d119666bc25b58a59f71d9ea221291e3d7a
Christian Pagtalunan <christianpagtalunan@gmail.com>
2026-09-08 18:23:52 +0800

    Fix:: ovelapping in select2bs5

--- resources/views/training_endorsement.blade.php
+++ resources/views/training_endorsement.blade.php
@@ -8,11 +8,6 @@
 @section('title', 'Training Endorsement')
 
 @section('content_page')
-<!-- Select2 CSS CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" />
-
-    <!-- Select2 Bootstrap 4 Theme CSS CDN -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@1.5.2/dist/select2-bootstrap4.min.css" />
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
