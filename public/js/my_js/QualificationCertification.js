b1d19394518b72bc8ce6b8ed94388720eef63133
Miguel C. Legaspi <mclegaspi@pricon.ph>
2026-09-16 10:03:16 +0800

    QC | TODO: Fix d-none bugs

--- public/js/my_js/QualificationCertification.js
+++ public/js/my_js/QualificationCertification.js
@@ -108,13 +108,14 @@
         $('.btnSaveInspector').addClass('d-none')
         $('#div_Oper').addClass('d-none');
         $('#divInspector').addClass('d-none');
+         $('#divSupervisor').addClass('d-none');
+        $('#divTechnician').addClass('d-none');
         // $('#productLine').addClass('d-none');
         $('#seriesDesignation').text('Series Name');
         $('#dateOfTransfer').addClass('d-none');
         $('.techSave').addClass('d-none');
         $('.btnSaveMatrix').addClass('d-none');
-        $('#divSupervisor').addClass('d-none');
-        $('#divTechnician').addClass('d-none');
+
         $('.saveSep').addClass('d-none');
         if(positionCategory === 'Supervisor'){
             $('.saveSep').removeClass('d-none');
